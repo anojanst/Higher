df2f76a6095366c45dd24337670add6b22d0c928
Samson <samsonjeyanthan5@gmail.com>
2024-02-23 21:06:24 +0530

    CU-86ennrjnk Added new file MobileNav.tsx for Mobile navigation

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,3 +3,4 @@ export { default as Button } from "./Button";
 export { default as Footer } from "./Footer";
 export { default as FormInput } from "./FormInput";
 export { default as Dropdown } from "./Dropdown";
+export { default as MobileNav } from "./MobileNav";
