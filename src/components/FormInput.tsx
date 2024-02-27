ea6fc5757bb93b3bc872b1ae8d0af6882cde280c
Samson <samsonjeyanthan5@gmail.com>
2024-02-27 19:57:34 +0530

    CU-86enqcpg1 Styles added and modification

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 
 interface FormInputProps {
-  label: string;
+  label?: string;
+  placeHolder?: string;
+  width?: string;
 }
 
-const FormInput: React.FC<FormInputProps> = ({ label }) => {
+const FormInput: React.FC<FormInputProps> = ({ label, placeHolder, width }) => {
   return (
-    <div className="flex flex-col w-full gap-2">
+    <div className={`flex flex-col w-[${width}] gap-2`}>
       <label className="text-gray-600 text-sm">{label}</label>
-      <input type="text" className="w-full rounded-md h-9 p-2 text-base" />
+      <input
+        type="text"
+        className="w-full rounded-md h-9 p-2 py-4 text-base"
+        placeholder={placeHolder}
+      />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         "primary-500": "#2980B9",
         "primary-600": "rgba(41, 127, 185, 0.5)",
         "secondary-500": "#F39C12",
+        "secondary-600": "rgba(243, 157, 18, 0.7)",
         "white-500": "#F6F6F6",
         "gray-500": "#ABABAB",
       },
