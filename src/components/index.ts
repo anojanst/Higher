fd303eddd01345ed2af3aecaf5bad4111637eb37
Samson <samsonjeyanthan5@gmail.com>
2024-03-02 21:43:13 +0530

    CU-86ennrjnk Files extenstion change .tsx to .ts

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as Navbar } from "./Navbar";
+export { default as Button } from "./Button";
+export { default as Footer } from "./Footer";
+export { default as FormInput } from "./FormInput";
+export { default as Dropdown } from "./Dropdown";
+export { default as MobileNav } from "./MobileNav";
+export { default as CheckBox } from "./CheckBox";
+export { default as CourseOverview } from "./CourseOverview";
